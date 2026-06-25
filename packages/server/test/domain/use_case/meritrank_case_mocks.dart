4869c018e0320dfee408730a603ed3d75f7e8225
V.G. Bulavintsev <golem.md@gmail.com>
2026-06-25 11:53:47 +0200

    test(cov-100): MeritRankCase unit tests

---
+++ packages/server/test/domain/use_case/meritrank_case_mocks.dart
@@ -0,0 +1,7 @@
+import 'package:mockito/annotations.dart';
+
+import 'package:tentura_server/domain/port/meritrank_repository_port.dart';
+import 'package:tentura_server/domain/port/user_repository_port.dart';
+
+@GenerateMocks([UserRepositoryPort, MeritrankRepositoryPort])
+void main() {}
