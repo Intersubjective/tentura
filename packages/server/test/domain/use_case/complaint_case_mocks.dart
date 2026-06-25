650bf28908c5f5b65cb80612dd3234ba2eb8eb48
V.G. Bulavintsev <golem.md@gmail.com>
2026-06-25 11:53:48 +0200

    test(cov-142): ComplaintCase unit tests

---
+++ packages/server/test/domain/use_case/complaint_case_mocks.dart
@@ -0,0 +1,8 @@
+import 'package:mockito/annotations.dart';
+
+import 'package:tentura_server/domain/port/complaint_repository_port.dart';
+
+@GenerateMocks([
+  ComplaintRepositoryPort,
+])
+void main() {}
