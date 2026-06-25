c7d156d32b6cc8d552dc626b708428dc6e4d1516
V.G. Bulavintsev <golem.md@gmail.com>
2026-06-25 11:53:47 +0200

    test(cov-103): MutualFriendsCase unit tests

---
+++ packages/server/test/domain/use_case/mutual_friends_case_mocks.dart
@@ -0,0 +1,8 @@
+import 'package:mockito/annotations.dart';
+
+import 'package:tentura_server/domain/port/mutual_friends_repository_port.dart';
+
+@GenerateMocks([
+  MutualFriendsRepositoryPort,
+])
+void main() {}
