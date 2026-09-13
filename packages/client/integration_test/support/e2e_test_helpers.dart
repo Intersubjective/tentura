ff78b0ee1e3338c9102f4ecc58bab25bb44fb8d2
V.G. Bulavintsev <golem.md@gmail.com>
2026-09-13 21:29:10 +0200

    fix(integration): import pointer device kind for map tap proof

--- packages/client/integration_test/support/e2e_test_helpers.dart
+++ packages/client/integration_test/support/e2e_test_helpers.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:js_interop';
-import 'dart:ui' show Offset, PlatformDispatcher;
+import 'dart:ui' show Offset, PlatformDispatcher, PointerDeviceKind;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
